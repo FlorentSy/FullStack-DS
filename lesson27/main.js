5e0c21a14b9ae667b5769406556dbd5cbe3ab610
FlorentSy <florentsylejmani658@gmail.com>
2024-11-04 12:11:09 +0100

    start of if else

---
+++ lesson27/main.js
@@ -0,0 +1,29 @@
+//conditionals
+var x=10;
+var y=5;
+
+//10>5 --yes
+
+if(x>y){
+    console.log("this is true");
+}else{
+    console.log("this is wrong");
+}
+
+if(x<y){
+    console.log("This is wrong")
+}else{
+    console.log("This is true");
+}
+
+if(y>x){
+    console.log("this is true")
+}else{
+    console.log("This is wrong");
+}
+
+if(x==7){
+    console.log("10 is equal 7")
+} else{
+    console.log("10 does not equal 7");
+}
