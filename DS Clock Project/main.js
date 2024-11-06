a6de36c1af173b3e067b03c5787a16add9301f24
FlorentSy <florentsylejmani658@gmail.com>
2024-11-06 22:48:00 +0100

    .

--- DS Clock Project/main.js
+++ DS Clock Project/main.js
@@ -1,9 +1,8 @@
-var noon=12;
-var wakeuptime;
-var sleeptime;
-var dstime;
-
-function showCurrentTime(){
+var noon = 12;
+var wakeuptime = 6;  // Default values
+var dstime = 15;  // Default values
+var sleeptime = 22;  // Default values
+function showCurrentTime() {
     var clock = document.getElementById("clock");
     var currentTime = new Date();
 
@@ -11,62 +10,57 @@ function showCurrentTime(){
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
 
-    var meridian = "AM";
+    var meridian = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12; // Convert to 12-hour format
 
-    if(hours>= noon){
-        meridian="PM";
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
     }
 
-    var clockTime=hours + " : " + minutes + " : " + seconds+ " " + meridian;
+    var clockTime = hours + " : " + minutes + " : " + seconds + " " + meridian;
     clock.innerHTML = clockTime;
-    changeImage();
+    changeImage();  // Ensure image updates based on current time
 }
 
-var oneSecond = 1000;
+setInterval(showCurrentTime, 1000);
 
-setInterval(showCurrentTime, oneSecond);
+function changeImage() {
+    var time = new Date().getHours();
 
-function changeImage(){
-    var time= new Date().getHours();
-    console.log(time);
-
-    var image = "images/ds_clock.png"
-    var imageHTML=document.getElementById("timeImage");
+    var image = "images/ds_clock.png";
+    var imageHTML = document.getElementById("timeImage");
 
     // Check if the current time falls within the wake-up time and class time range
-    if (currentHour >= wakeuptime && currentHour < dstime) {
+    if ((wakeuptime <= dstime && time >= wakeuptime && time < dstime) ||
+        (wakeuptime > dstime && (time >= wakeuptime || time < dstime))) {
         image = "images/morning.gif";  // Morning image
-    } 
-    else if (currentHour >= dstime && currentHour < sleeptime) {
+    } else if ((dstime <= sleeptime && time >= dstime && time < sleeptime) ||
+               (dstime > sleeptime && (time >= dstime || time < sleeptime))) {
         image = "images/class.gif";  // Class image
-    } 
-    else if (currentHour >= sleeptime || currentHour < wakeuptime) {
+    } else {
         image = "images/night.gif";  // Night image
-    }
-
-    // Handle wrap-around for sleep time (e.g. 11:00 PM - 12:00 AM as morning)
-    if (sleeptime <= wakeuptime) {
-        // If sleep time is after midnight and before wakeup time (wraps around the day)
-        if (currentHour >= sleeptime || currentHour < wakeuptime) {
-            image = "images/morning.gif";  // Show morning image during this range
-        }
     }
 
     imageHTML.src = image;  // Update the image source
 }
 
+
 function updateClock() {
     // Get the selected times from the selectors
     var wakeUpTimeSelector = document.getElementById("wakeUpTimeSelector");
+    var dsTimeSelector = document.getElementById("DSTimeSelector");
+    var sleepTimeSelector = document.getElementById("sleepTimeSelector");
+
     wakeuptime = parseInt(wakeUpTimeSelector.value, 10);  // Store as integer
-
-    var dsTimeSelector = document.getElementById("dsTimeSelector");
     dstime = parseInt(dsTimeSelector.value, 10);  // Store as integer
-
-    var sleepTimeSelector = document.getElementById("sleepTimeSelector");  // Correct spelling
     sleeptime = parseInt(sleepTimeSelector.value, 10);  // Store as integer
+
+    changeImage();  // Update the image immediately after changing time settings
 }
 
-var saveButton= document.getElementById("saveButton");
-
+var saveButton = document.getElementById("saveButton");
 saveButton.addEventListener("click", updateClock);
+
