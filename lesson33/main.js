ea104d64060a69acea437df6ffc13b1d924ddf29
FlorentSy <florentsylejmani18@gmail.com>
2024-11-15 10:15:23 +0100

    ..

--- lesson33/main.js
+++ lesson33/main.js
@@ -1,15 +1,29 @@
-// js
-// document.getElementById("myElement").style.display = "none";
+//  js
+// // document.getElementById("myElement").style.display = "none";
 
-//jQuery
-$('myElement').hide();
+// //jQuery
+// $('#myElement').hide();
 
-//$('#myElement').text('NewText'); //HTML elements simple manipulation /replace txt/
+// //$('#myElement').text('NewText'); //HTML elements simple manipulation /replace txt/
 
-$('.btn').click(function(){
-    console.log($('#myElement').text());
+// $('.btn').click(function(){
+//     console.log($('#myElement').text());
 
-    $('#myElement').text('Testing text');
-    $('#myElement').append(' <br> Extra text');
-});
+//     $('#myElement').text('Testing text');
+//     $('#myElement').append(' <br> Extra text');
+// });
 
+$('#btn3').click(function(){
+    $('.hidden').show('slow');
+})
+
+$('#btn4').click(function(){
+    $('.hidden').hide();
+})
+
+$("#square").click(function(){
+    $("#square").animate({
+        'width' : '200px',
+        'height' : '200px',
+    })
+})
