6c53287cd7b7f279ed8359e2a8d3b0b4af437e73
FlorentSy <florentsylejmani18@gmail.com>
2024-11-25 11:18:02 +0100

    php functions, variables

--- PHPLesson41/index.php
+++ PHPLesson41/index.php
@@ -1,8 +1,32 @@
 <?php 
+
 $name = "Fl0rent";
-echo "I'am $name";
+echo "I'am $name <br>";
 
+$x = 50;
+$y = 30;
 
+echo $x + $y . "<br>";
+echo $x - $y . "<br>";
+echo $x * $y . "<br>";
+echo $x / $y . "<br>";
+
+$a = "Florent";
+$b = "Sy";
+
+$c = $a.$b . "<br>"; // string concatenation .
+echo "$c \n ";
+
+$message = "Hello";
+$message .= " World";
+
+echo $message. "<br>";
+
+$the_string = "Florent SY lejmani";
+echo strlen($the_string ). "<br>";
+echo str_word_count($the_string) . "<br>";
+echo str_replace("lejmani", "LEJMANI", $the_string) . "<br>";
+echo strrev($the_string);
 
 
 
