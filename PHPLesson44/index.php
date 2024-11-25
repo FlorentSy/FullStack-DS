eb8ba43687dfe08251a9ef02d3964ac1ce19088d
FlorentSy <florentsylejmani18@gmail.com>
2024-11-25 12:20:03 +0100

    functions, types, builtinfunctions

--- PHPLesson44/index.php
+++ PHPLesson44/index.php
@@ -1,8 +1,25 @@
 <?php
-echo "Lesson44";
+echo "Lesson44 <br>";
+// phpinfo();
+
+$x = "Hello <br>";
+print_r($x);
+
+$x = 5;
+echo gettype($x) . "<br>";
+
+$y = 10.07;
+echo gettype($y) . "<br>";
+
+$z = "Hello";
+echo gettype($z) . "<br>";
 
 
+function displayVersion(){
+    echo "this is your php version".phpversion();
+    echo "\n";
+}
 
-
+displayVersion();
 
 ?>
