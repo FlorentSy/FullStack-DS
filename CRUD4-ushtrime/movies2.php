fc1df68c97f22095eda980222f6ed88b5ce67247
FlorentSy <florentsylejmani18@gmail.com>
2024-12-17 22:47:57 +0100

    added movies

--- CRUD4-ushtrime/movies2.php
+++ CRUD4-ushtrime/movies2.php
@@ -5,15 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .card-text{
+            font-size: 14px;
+        }
+        .card-container {
+        display: flex;
+        flex-wrap: wrap;
+        }
+    </style>
 </head>
 <body>
-
-    <div class="card m-2" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
+        <div class="card-container">
+    <div class="card m-2" style="width: 25rem;">
+    <img class="card-img-top" src="https://cdn.sortiraparis.com/images/80/98390/979068-prison-break-la-serie-culte-des-annees-2000-bientot-de-retour-que-sait-on-de-ce-nouveau-projet.jpg"  alt="Card image cap" height="250px">
     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Prison Break</h5>
+        <p class="card-text">Architect Michael Scofield gets himself imprisoned to break out his wrongly convicted brother, Lincoln Burrows, who's on death row. Michael has the prison blueprints tattooed on his body, forming an intricate escape plan. Inside, they face corrupt guards, dangerous inmates, and a vast conspiracy. Their desperate race against time unfolds within and beyond the prison walls.</p>
+        <a href="#" class="btn btn-primary">Book Now</a>
+    </div>
+    </div>
+
+    <div class="card m-2" style="width: 25rem;">
+    <img class="card-img-top" src="https://boredanddangerousblog.wordpress.com/wp-content/uploads/2015/09/beautiful-mind.jpg"  alt="Card image cap" height="250px">
+    <div class="card-body">
+        <h5 class="card-title">A Beautiful Mind </h5>
+        <p class="card-text">"A Beautiful Mind" portrays the life of John Nash, a brilliant but troubled mathematician. He makes groundbreaking discoveries while battling schizophrenia. His genius and fragile mental state lead him on a captivating journey of self-discovery and redemption, as he strives to maintain his sanity and contribute to the world.</p>
+        <a href="#" class="btn btn-primary">Book Now</a>
+    </div>
     </div>
     </div>
     <h1>Movies for users</h1>
